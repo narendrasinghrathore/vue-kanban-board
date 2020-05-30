76993135ca43b348047f27b1da74a710711185dd
narendrasinghrathore <rathoreofficialemail@gmail.com>
2020-05-30 17:11:34 +0530

    Initial setup

---
+++ src/components/Boards.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Boards</h1>
+    <ul>
+      <li>Some item here</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      m: 'Hi there'
+    }
+  }
+}
+</script>
+
+<style></style>
